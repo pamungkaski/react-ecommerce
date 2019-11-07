bd832c685963bac4913f70023340c4294beb0157
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2019-11-07 19:03:30 +0700

    fix 200 image

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -6,11 +6,37 @@ import './styles.sass';
 
 class Item extends Component {
   render() {
-  	let img
-  	if (this.props.nomor < 5) {
-  		img = <img className="image" src={require("../../assets/images/sneakers_191107_000"+(this.props.nomor+1).toString(10)+".jpg")}/>
-  	} else {
-  		img = <img className="image" src={require("../../assets/images/Hat_191107_000"+(this.props.nomor-4).toString(10)+".jpg")}/>
+  	let img = <img className="image" src={require("../../assets/images/sneakers_191107_000"+(this.props.nomor+1).toString(10)+".jpg")}/>
+  	
+  	let naom = Math.floor(this.props.nomor / 4 )
+  	
+  	if (this.props.numor % 4 == 1) {
+  		img = <img className="image" src={require("../../assets/images/sneakers_191107_000"+(naom+1).toString(10)+".jpg")}/>
+  	
+  		if naom >= 10 {
+  			img = <img className="image" src={require("../../assets/images/sneakers_191107_00"+(+1).toString(10)+".jpg")}/>
+  		}
+  	}
+  	if (this.props.numor % 4 == 2) {
+  		img = <img className="image" src={require("../../assets/images/Bag_191107_000"+(naom+1).toString(10)+".jpg")}/>
+  	
+  		if naom >= 10 {
+  			img = <img className="image" src={require("../../assets/images/Bag_191107_00"+(naom+1).toString(10)+".jpg")}/>
+  		}
+  	}
+  	if (this.props.numor % 4 == 3) {
+  		img = <img className="image" src={require("../../assets/images/sHat_191107_000"+(naom+1).toString(10)+".jpg")}/>
+  	
+  		if naom >= 10 {
+  			img = <img className="image" src={require("../../assets/images/Hat_191107_00"+(naom+1).toString(10)+".jpg")}/>
+  		}
+  	}
+  	if (this.props.numor % 4 == 0) {
+  		img = <img className="image" src={require("../../assets/images/Belt_191107_000"+(naom+1).toString(10)+".jpg")}/>
+  	
+  		if naom >= 10 {
+  			img = <img className="image" src={require("../../assets/images/Belt_191107_00"+(naom+1).toString(10)+".jpg")}/>
+  		}
   	}
     return(
       <div className="item" style={{display: 'flex', justifyContent: 'center'}}>
